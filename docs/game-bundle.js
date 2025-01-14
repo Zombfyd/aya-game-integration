ed7bba158b7baf65a4ec29e0e815a1644e0275fb
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-14 11:35:49 -0800

    Create game-bundle.js

---
+++ docs/game-bundle.js
@@ -0,0 +1,5 @@
+// This is your bundled game code
+// We'll replace this with the properly bundled version later
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('Game bundle loaded successfully');
+});
