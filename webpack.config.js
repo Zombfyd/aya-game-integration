0dbbc50889b902555ed591612a54d4ff456da3f6
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-14 17:13:28 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,31 +7,30 @@ module.exports = {
   output: {
     filename: 'game-bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/',  // Ensure this is needed for your project setup
   },
 
   // Use source map only in production environment
   devtool: process.env.NODE_ENV === 'production' ? 'source-map' : false,
 
   module: {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
       },
-    },
-    {
-      test: /\.css$/i,   // This is the correct way to specify the test
-      use: ['style-loader', 'css-loader'],
-    },
-  ],
-},
-
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -52,5 +51,16 @@ module.exports = {
     minimize: process.env.NODE_ENV === 'production', // Minify in production
   },
 
-  mode: 'production', // Force production mode
+  mode: process.env.NODE_ENV || 'development',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    client: {
+      logging: 'none',  // Suppresses "Server started" messages
+    },
+    hot: false,  // Disable hot module replacement
+    liveReload: false,  // Disable live reload
+  },
 };
