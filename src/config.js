7348eed965ffe271fd306e0a6107e1345062dc40
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-17 12:20:31 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     development: {
-        packageId: process.env.REACT_APP_PACKAGE_ID || 'YOUR_LOCAL_PACKAGE_ID',
-        ownerAddress: process.env.REACT_APP_OWNER_ADDRESS || 'YOUR_LOCAL_OWNER_ADDRESS',
+        packageId: process.env.0x4bfa52ee471bd01ea0ade83a343de62a4c500f9adc375eb4426a92042887b13d || 'YOUR_LOCAL_PACKAGE_ID',
+        ownerAddress: process.env.0x715c980c14bbbbbec3bfdb8eb0388f47d1e89a4a63e07355d3f38fec173396af || 'YOUR_LOCAL_OWNER_ADDRESS',
         network: 'devnet'
     },
     production: {
